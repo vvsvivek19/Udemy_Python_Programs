af38ef9ae57486f38294be6a347aebaa24de6afa
Vivek Singh <vvsvivek19@gmail.com>
2024-10-09 06:01:30 +0530

    Minor change

--- Conditionals/Leap_year.py
+++ Conditionals/Leap_year.py
@@ -1,11 +1,11 @@
 year = int(input("Please enter a year: "))
 
-if year % 100 == 0:
+if year % 100 == 0: #this if is for checking years multiple of 100 and 400
     if year % 400 == 0:
         print("It is a leap year!")
     else:
         print("Not a leap year!")
-elif year % 4 == 0:
+elif year % 4 == 0: #To check for years which are not multiples of 100 or 400
     print("It is a leap year!")
 else:
     print("Not a leap year!")
